821c1ae9e4098eabd776b3df0bb7c7c18bd254ba
Keller Me <ppmiao0628@outlook.com>
2021-02-24 22:42:08 +0800

    更新打包

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./dist/index.min.js')
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./dist/index.min.js')
+} else {
+    module.exports = require('./dist/index.js')
+}
